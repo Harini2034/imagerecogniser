d54cb7928937ee9ba865a89b57255492e70cedff
Harini2034 <72262425+Harini2034@users.noreply.github.com>
2021-03-23 18:29:41 +0530

    Update main.js

--- main.js
+++ main.js
@@ -20,6 +20,11 @@ function take_snapshot()
 console.log('ml5 version', ml5.version);
 classifier = ml5.imageClassifier('MobileNet', modelLoaded);
 
+function modelLoaded()
+{
+    console.log('Model Loaded!');
+}
+
 function check()
  {
      img = document.getElementById('captured_image');
